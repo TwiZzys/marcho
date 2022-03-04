08331bb7c035f9e44b996747f2cd170e20d06f5a
Vitalii <witaliy_sapriga@ukr.net>
2022-03-04 19:26:53 +0200

    added product page p.1

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,24 @@
 $(function () {
+  $('.product-slide__thumb').slick({
+    asNavFor: '.product-slide__big',
+    focusOnSelect: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    vertical: true,
+    draggable: false
+  });
+  $('.product-slide__big').slick({
+    asNavFor: '.product-slide__thumb',
+    draggable: false,
+    arrows: false,
+    fade: true 
+  });
   $('.shop-content__filter-btn').on('click', function () {
     $('.shop-content__filter-btn').removeClass('shop-content__filter-btn--active');
     $(this).addClass('shop-content__filter-btn--active');
   });
 
-  $('.btn-list').on('click', ()=>{
+  $('.btn-list').on('click', () => {
     $('.product-item').addClass('product-item--list');
   });
 
@@ -12,7 +26,7 @@ $(function () {
     $('.product-item').removeClass('product-item--list');
   });
 
-  $('.select-styler').styler();
+  $('.select-styler, .product-one__item-num').styler();
   $('.filter-price__input').ionRangeSlider({
     type: 'double',
     prefix: '$',
